58c6d0d7225f28212f3f2b9bcbe7695559972a05
William Gregory <reachwill@Williams-MBP.home>
2015-09-01 20:05:35 +0100

    Tracks and updates an active project. Resources can be added to active project but not yet persistent.

---
+++ js/clipper/managers/ProjectManager.js
@@ -0,0 +1,49 @@
+ProjectManager = {
+
+    activeProject: {
+        resources: [],
+        cliplists: [],
+        members: [],
+        description: '',
+        title: 'Untitled Project',
+        id: null
+    },
+
+    setActiveProject: function (resources, cliplists, members, description, title, id) {
+        var self = ProjectManager;
+        //set default values for parameters
+        resources = typeof resources !== 'undefined' ? resources : [];
+        cliplists = typeof cliplists !== 'undefined' ? cliplists : [];
+        members = typeof members !== 'undefined' ? members : [];
+        description = typeof description !== 'undefined' ? description : '';
+        title = typeof title !== 'undefined' ? title : 'Untitled Project';
+        id = typeof id !== 'undefined' ? id : null;
+
+
+        //apply project parameters to active project
+        self.activeProject.resources = resources;
+        self.activeProject.cliplists = cliplists;
+        self.activeProject.members = members;
+        self.activeProject.description = description;
+        self.activeProject.title = title;
+        self.activeProject.id = id;
+    },
+
+    updateProjectProp: function (prop, value) {
+        var self = ProjectManager;
+        if (Object.prototype.toString.call(self.activeProject[prop]) === '[object Array]') {
+            self.activeProject[prop].push(value);
+        } else {
+            self.activeProject[prop] = value;
+        }
+        switch (prop) {
+            case 'resources':
+                View.updateResourceList();
+                break;
+        }
+    }
+
+}
+
+ProjectManager.setActiveProject()
+    //ProjectManager.updateProjectProp('resources', ['yehah']);
